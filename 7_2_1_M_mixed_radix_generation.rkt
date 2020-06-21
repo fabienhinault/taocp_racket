42eca6ae0659cbf45f718b3eb3b3dc4f4734e0ec
Fabien Hinault <fabien.hinault@free.fr>
2020-06-21 16:18:10 +0200

    mixed_radix_generation reverse order

--- 7_2_1_M_mixed_radix_generation.rkt
+++ 7_2_1_M_mixed_radix_generation.rkt
@@ -12,16 +12,21 @@
 
 (define (__generate rs gd proc)
     (proc gd)
-    (to-next rs gd (sub1 (length rs)) proc))
+    (to-next rs gd 0 proc))
 
 (define (to-next rs gd j proc)
-  (when (not (equal? j -1))
+  (when (not (equal? j (length rs)))
     (if (equal? (list-ref gd j) (sub1 (list-ref rs j)))
-        (to-next rs (list-set gd j 0) (sub1 j) proc)
+        (to-next rs (list-set gd j 0) (add1 j) proc)
         (__generate rs (list-set gd j (add1 (list-ref gd j))) proc))))
 
+;(time (generate '(2 9 5 3 4 6) println))
+;cpu time: 3056 real time: 3054 gc time: 486
+;(time (generate '(7 2 9 5 3 4 6) println))
+;cpu time: 23187 real time: 23157 gc time: 3130
+
 (let* ((void-mock (mock #:behavior void)))
-  (to-next '(1) '(0) -1 void-mock)
+  (to-next '(1) '(0) 1 void-mock)
   (check-mock-num-calls void-mock 0))
 
 (let* ((void-mock (mock #:behavior void)))
@@ -46,18 +51,18 @@
 
 (let* ((void-mock (mock #:behavior void)))
   (generate '(2 2) void-mock)
-  (check-mock-calls void-mock (list (arguments '(0 0)) (arguments '(0 1)) (arguments '(1 0))  (arguments '(1 1)) )))
+  (check-mock-calls void-mock (list (arguments '(0 0)) (arguments '(1 0)) (arguments '(0 1))  (arguments '(1 1)) )))
 
 (let* ((void-mock (mock #:behavior void)))
   (generate '(2 3) void-mock)
-  (check-mock-calls void-mock (list (arguments '(0 0)) (arguments '(0 1)) (arguments '(0 2))
-                                    (arguments '(1 0)) (arguments '(1 1)) (arguments '(1 2)))))
+  (check-mock-calls void-mock (list (arguments '(0 0)) (arguments '(1 0))
+                                    (arguments '(0 1)) (arguments '(1 1))
+                                    (arguments '(0 2)) (arguments '(1 2)))))
 
 (let* ((void-mock (mock #:behavior void)))
   (generate '(3 2) void-mock)
-  (check-mock-calls void-mock (list (arguments '(0 0)) (arguments '(0 1))
-                                    (arguments '(1 0)) (arguments '(1 1))
-                                    (arguments '(2 0)) (arguments '(2 1)))))
+  (check-mock-calls void-mock (list (arguments '(0 0)) (arguments '(1 0)) (arguments '(2 0))
+                                    (arguments '(0 1)) (arguments '(1 1)) (arguments '(2 1)))))
 
 
 
